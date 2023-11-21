ebad85b5234772bddaa8d98a70a4626ed8f9ba53
Shahil <sahilaryan9211@gmail.com>
2023-11-21 16:40:47 +0530

    first commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,16 @@
+export const WorkData = [
+  {
+    title: "StageSplash",
+    desc: "A singer hiring website with a touch of creativity.",
+    tech: [
+      "React.js",
+      "Firebase",
+      "TailwindCss",
+      "APIs",
+      "Axios",
+      "Context API",
+    ],
+    gitlink: "https://stag-ex-splash.vercel.app/",
+    site: "https://stag-ex-splash.vercel.app/",
+  },
+];
